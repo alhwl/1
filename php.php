a4c677ebd488410d71744e9c9bd2544f0955b48e
alhwl <143041398+alhwl@users.noreply.github.com>
2023-11-10 09:41:45 -0600

    Update and rename php.php to phptitle.php

--- php.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "b1n4ry";
-?>
---
+++ phptitle.php
@@ -0,0 +1,16 @@
+<?php 
+
+$title = "b1n4ry";
+
+?>
+
+<html>
+<head>
+<title><?php echo $title; ?></title> 
+</head>
+<body>
+
+<h1><?php echo $title; ?></h1>
+
+</body>
+</html>
